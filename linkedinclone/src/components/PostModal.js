0aa3d4acb9b00d57a467916f15fed20fc115b58a
Javier Villarreal <jvillarreal2016@gmail.com>
2021-06-08 17:38:01 -0500

    posting module functionality working

--- linkedinclone/src/components/PostModal.js
+++ linkedinclone/src/components/PostModal.js
@@ -1,11 +1,34 @@
 import styled from 'styled-components';
 import { useState } from "react";
+import ReactPlayer from "react-player";
+import { connect } from "react-redux";
 
 const PostModal = (props) => {
     const [editorText, setEditorText] = useState("");
+    const[shareImage, setShareImage] = useState("");
+    const [videoLink, setVideoLink] = useState("");
+    const [assetArea, setAssetArea] = useState("");
+
+    const handleChange = (e) => {
+        const image = e.target.files[0];
+
+        if(image === '' || image===undefined) {
+            alert(`not an image, the file is a ${typeof image}`);
+            return;
+        }
+        setShareImage(image);
+    };
+
+    const switchAssetArea = (area) => {
+        setShareImage("");
+        setVideoLink('');
+        setAssetArea(area);
+    };
 
     const reset = (e) => {
-        setEditorText("")
+        setEditorText("");
+        setShareImage("");
+        setVideoLink("");
         props.handleClick(e);
     };
     return (
@@ -22,23 +45,58 @@ const PostModal = (props) => {
             </Header>
             <SharedContent>
                 <UserInfo>
-                    <img src="/images/user.svg" />
-                    <span>Name</span>
+                    {props.user.photoURL ? (  //does photo exist
+                    <img src={props.user.photoURL} /> //if photo exists show photo
+                    ) : (
+                    <img src="/images/user.svg" /> //otherwise show anonymous user photo
+                    )}
+                    <span>{props.user.displayName}</span>
                 </UserInfo>
                 <Editor>
                     <textarea value={editorText}
                     onChange={(e) => setEditorText(e.target.value)}
                     placeholder="What do you want to talk about?"
                     autoFocus={true}
-                    ></textarea>
+                    />
+                    { assetArea === 'image' ?
+
+                    <UploadImage>
+                        <input type="file"
+                        accept='image/gif, image/jpeg, image/png'
+                        name="image"
+                        id="file"
+                        style={{display: 'none'}}
+                        onChange={handleChange}
+                    />
+                    <p>
+                        <label htmlFor="file">
+                            Select an image to share
+                        </label>
+                    </p>
+                    {shareImage && <img src={URL.createObjectURL(shareImage)} />}
+                    </UploadImage>
+                    :
+                    assetArea === 'media' &&
+                    <>
+                    <input 
+                        type="text"
+                        placeholder="Place input a video link"
+                        value={videoLink}
+                        onChange={(e) => setVideoLink(e.target.value)}
+                    />
+                    {videoLink && (
+                         <ReactPlayer width={'100%'} url={videoLink} />
+                    )}
+                    </>
+                }
                 </Editor>
             </SharedContent>
             <SharedCreation>
                 <AttachAssets>
-                    <AssetButton>
+                    <AssetButton onClick={() => switchAssetArea("image")}>
                         <img src="/images/share-image.svg" alt="" />
                     </AssetButton>
-                    <AssetButton>
+                    <AssetButton onClick={() => switchAssetArea("media")}>
                         <img src="/images/share-img.svg" alt="" /> 
                     </AssetButton>
                 </AttachAssets>
@@ -200,4 +258,20 @@ const Editor = styled.div`
     }
 `;
 
-export default PostModal;
+const UploadImage = styled.div`
+    text-align: center;
+    img {
+        width: 100%;
+
+    }
+`;
+
+const mapStateToProps = (state) => {
+    return {
+        user: state.userState.user,
+    };
+};
+
+const mapDispatchToProps = (dispatch) => ({});
+
+export default connect(mapStateToProps, mapDispatchToProps)(PostModal);
